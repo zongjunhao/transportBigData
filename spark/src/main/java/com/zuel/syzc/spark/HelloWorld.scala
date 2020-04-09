36f7016d5dbd79bbc84e170dbfcf8e9c2cb08cc5
crescent73 <1987543919@qq.com>
2020-04-09 16:32:05 +0800

    导入数据

--- spark/src/main/java/com/zuel/syzc/spark/HelloWorld.scala
+++ spark/src/main/java/com/zuel/syzc/spark/HelloWorld.scala
@@ -1,7 +1,18 @@
 package com.zuel.syzc.spark
 
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
 object HelloWorld {
   def main(args: Array[String]): Unit = {
     println("Hello World!")
+    val sparkConf = new SparkConf().setMaster("local[*]").setAppName("analysis")
+    val spark: SparkSession = SparkSession.builder().config(sparkConf).getOrCreate()
+    spark.read.format("csv").option("header","true").load("in/服创大赛-原始数据.csv").createOrReplaceTempView("raw_data")
+    spark.sql("select * from raw_data").show()
+    spark.read.format("csv").option("header","true").load("in/服创大赛-基站经纬度数据.csv").createOrReplaceTempView("longitude")
+    spark.sql("select * from longitude").show()
+    spark.read.format("csv").option("header","true").load("in/服创大赛-出行方式静态数据.csv").createOrReplaceTempView("way")
+    spark.sql("select * from way").show()
   }
 }
