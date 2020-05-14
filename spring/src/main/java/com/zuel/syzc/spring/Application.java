ce9fc1e47774529660c2f9db2e7eb77049ab2579
谈腊云 <2462289249@qq.com>
2020-05-14 12:36:24 +0800

    html

--- spring/src/main/java/com/zuel/syzc/spring/Application.java
+++ spring/src/main/java/com/zuel/syzc/spring/Application.java
@@ -2,11 +2,13 @@ package com.zuel.syzc.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(Application.class, args);
     }
 
