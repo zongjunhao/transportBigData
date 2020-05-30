be615ebc22ff9f4ba6f74a5cc7e5857f1df1363a
wangzhengyang <zy_wao@163.com>
2020-05-30 22:28:16 +0800

    js login register

--- spring/src/main/resources/static/js/login.js
+++ spring/src/main/resources/static/js/login.js
@@ -40,37 +40,24 @@ function user_login(){
     if(flag){
         $.ajax({
             type : "POST",
-            url : "/KnowledgeLibrary/user/login",
+            url : "/system/login",
             data : {
-                u_mail : $("#user_name").val(),
-                u_pwd : $("#user_password").val(),
-                inputRandomCode : $("#user_inputRandomCode").val()
+                name : $("#user_name").val(),
+                password : $("#user_password").val()
             },
             success : function(json) {
                 console.log(json)
-                if (json.resultCode == "4000") {
+                if (json.code == "200") {
                     //登录成功
                     //alert(json.resultDesc);
-                    $.session.set("u_age",json.data.u_age);
-                    $.session.set("u_mail",json.data.u_email);
-                    $.session.set("u_firm",json.data.u_firm);
-                    $.session.set("u_gender",json.data.u_gender);
-                    $.session.set("u_id",json.data.u_id);
-                    $.session.set("u_name",json.data.u_name);
-                    $.session.set("u_phone",json.data.u_phone);
-                    $.session.set("u_pwd",json.data.u_pwd);
-                    $.session.set("u_role",json.data.u_role);
-                    let role=json.data.u_role+1;
-                    console.log(role)
-                    $.session.set("role", role);
-                    console.log($.session.get("role"));
-                    window.location.href = "index.html";
+                    $.session.set("u_id",json.data.id);
+                    $.session.set("u_name",json.data.name);
+                    window.location.href = "main.html";
                 } else {
                     //登录失败
                     //alert(json.resultDesc);
-                    $("#user_code_tip").text(json.resultDesc);
+                    $("#user_code_tip").text(json.msg);
                     $("#user_code_tip").css("color", "#F05A5A");
-                    $("#user_pic").attr('src','user/pic?x='+Math.random()); 
                 }
             },
             error : function(jqXHR) {
